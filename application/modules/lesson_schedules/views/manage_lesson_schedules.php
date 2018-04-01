a70e2fe8eeddf44a7d302a3d15ba22b6695f31dd
Suguru Tokuda <suguru.tokuda@gmail.com>
2018-04-01 13:07:17 -0500

    Added get_time in Timedate class to convert UNIX timestamp to AM and PM. Now Lesson_schedules uses this to convet start_date and end_date into date and time

--- application/modules/lesson_schedules/views/manage_lesson_schedules.php
+++ application/modules/lesson_schedules/views/manage_lesson_schedules.php
@@ -29,15 +29,15 @@ if (isset($flash)) {
           $this->load->module('timedate');
           $edit_lesson_schedule_url = base_url()."lesson_schedules/create_lesson_schedule/".$lesson_id.'/'.$row->id;
           $lesson_schedule_id = $row->id;
-          $lesson_date = $this->timedate->get_date($row->lesson_date, "datepicker_us");
-          $start_time = $row->lesson_start_time;
-          $end_time = $row->lesson_end_time;
+          $lesson_date = $this->timedate->get_date($row->lesson_start_date, "datepicker_us");
+          $lesson_start_time = $this->timedate->get_time($row->lesson_start_date);
+          $lesson_end_time = $this->timedate->get_time($row->lesson_end_date);
           $num_of_bookings = $this->lesson_bookings->_get_num_of_bookings_for_lesson_schedule_id($lesson_schedule_id);
           ?>
           <tr>
             <td><?= $lesson_date ?></td>
-            <td><?= $start_time ?></td>
-            <td><?= $end_time ?></td>
+            <td><?= $lesson_start_time ?></td>
+            <td><?= $lesson_end_time ?></td>
             <td><?= $num_of_bookings?></td>
             <td class="center">
               <a class="btn btn-success" href="<?= $edit_lesson_schedule_url ?>">
