329e55160a26e6a31eeecbcbdf460cd133eba126
suguru-tokuda <suguru.tokuda@gmail.com>
2018-04-23 22:23:51 -0500

    Adding additional changes to UI

--- application/modules/templates/views/top_bar.php
+++ application/modules/templates/views/top_bar.php
@@ -5,7 +5,15 @@
     </a>
     <a class="hidden-md-down" href="tel:00331697720">
       <i class="icon-bell"></i>&nbsp; 00 33 169 7720</a>
-      <a class="social-button sb-facebook shape-none sb-dark" href="#" target="_blank"><i class="socicon-facebook"></i></a><a class="social-button sb-twitter shape-none sb-dark" href="#" target="_blank"><i class="socicon-twitter"></i></a><a class="social-button sb-instagram shape-none sb-dark" href="#" target="_blank"><i class="socicon-instagram"></i></a><a class="social-button sb-pinterest shape-none sb-dark" href="#" target="_blank"><i class="socicon-pinterest"></i></a>
+      <a class="social-button sb-facebook shape-none sb-dark" href="#" target="_blank">
+        <i class="socicon-facebook"></i>
+      </a>
+      <a class="social-button sb-twitter shape-none sb-dark" href="#" target="_blank">
+        <i class="socicon-twitter"></i>
+      </a>
+      <a class="social-button sb-instagram shape-none sb-dark" href="#" target="_blank">
+        <i class="socicon-instagram"></i>
+      </a>
   </div>
   <div class="topbar-column">
   </div>
